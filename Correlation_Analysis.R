dc8b20c4a895365e637dbbfa5464e0f793551b73
Mohamed1706 <88836531+Mohamed1706@users.noreply.github.com>
2024-03-19 14:49:13 +0100

    Added file for correlation analysis

--- Correlation_Analysis.R
+++ Correlation_Analysis.R
@@ -12,3 +12,16 @@ library(ggcorrplot)
 plz2017 <- read.csv("./Datasets/PLZ_2017.csv", sep = ";", encoding = "latin1")
 plz2021 <- read.csv("./Datasets/PLZ_2021.csv", sep = ";", encoding = "latin1")
 
+# Columns to analyse 
+correlation_columns <- intersect(names(plz2017), names(plz2021))
+n_correlation_columns <- length(correlation_columns)
+
+
+# TODO: (Mohamed)
+
+correlation_columns_mohamed <- correlation_columns[seq(from = 1, by = 1, to = n_correlation_columns / 2)]
+
+# TODO: (Ilija)
+
+correlation_columns_ilija <- correlation_columns[seq(from = (n_correlation_columns/2) + 1, by = 1, to = n_correlation_columns)]
+
