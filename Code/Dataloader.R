4ab5b3f0b3cfc1afd6770bcafed6f82cd9d96e9f
Mohamed1706 <mohamed.tchakala@outlook.de>
2024-04-09 10:55:43 +0200

    Adjusted code for explorative analysis, add code for mapping ecar registrations, add data loader which is supposed to be used accross all additional script

---
+++ Code/Dataloader.R
@@ -0,0 +1,43 @@
+# packages
+library(dplyr)
+
+right <- function(x, n) {
+  substr(x, nchar(x) - n + 1, nchar(x))
+}
+
+
+read_data <- function(year) {
+  #Read data
+  data <- read.csv(paste("./Datasets/PLZ_",year,".csv", sep = ""), sep = ";", encoding = "latin1")
+  names(data) <- tolower(names(data))
+  
+  #Merge covariates from 2017 
+  if(year %in% as.character(2018:2020)) {
+    data2017 <- read.csv("./Datasets/PLZ_2017.csv", encoding = "latin1", sep = ";")
+    
+    # Aggregate 81248 and 81249
+    data$plz <- as.character(data$plz)
+    data2017$plz <- as.character(data2017$plz)
+    
+    if(any(data$plz %in% "81248")) {
+      data <- data %>% replace(is.na(.), 0)
+      colums_to_aggregate <- c("plz5_kba", "plz5_kba_kraft3")
+      data[data$plz == "81249", colums_to_aggregate] <- colSums(data[data$plz %in% c("81248", "81249"), colums_to_aggregate])
+      data <- data[data$plz != "81248", ]
+    }
+    
+    # Merge data
+    result <- left_join(x = data, y = data2017[, !names(data2017) %in% c("plz5_kba_kraft3", "plz5_kba", "ort")], by = "plz")
+  }
+  
+  else if(year %in% as.character(c(2017,2021))) {
+    result <- data
+  }
+  result
+}
+
+plz2017 <- read_data("2017")
+plz2018 <- read_data("2018")
+plz2019 <- read_data("2019")
+plz2020 <- read_data("2020")
+plz2021 <- read_data("2021")
--- Code/Explorative_Analysis.R
+++ Code/Explorative_Analysis.R
@@ -9,16 +9,14 @@ library(tidyverse)
 library(ggcorrplot)
 library(mgcv)
 library(lme4)
-
+library(glmnet)
+library(reshape2)
 
 # Datasets
+source("./Code/Dataloader.R")
 
-plz2017 <- read.csv("./Datasets/PLZ_2017.csv", sep = ";", encoding = "latin1")
-plz2021 <- read.csv("./Datasets/PLZ_2021.csv", sep = ";", encoding = "latin1")
 df_quality_check_2017 <- read.xlsx("./Datasets/DataQualityCheck.xlsx", sheet = "2017")
 df_quality_check_2021 <- read.xlsx("./Datasets/DataQualityCheck.xlsx", sheet = "2021")
-
-# TODO: First model analysis of known predictors of ecars registrations
 
 #' Categorized all covariates into four groups based on findings in (Wappelhorst, 2022)
 columns.wappelhorst <- df_quality_check_2017$column[!is.na(df_quality_check_2017$category)]
@@ -39,6 +37,7 @@ get_formula <- function(covariates, offset = NULL, target = "plz5_kba_kraft3") {
   }
   result
 }
+
 # fit of a poisson model
 poisson_fit <- function(formula, data) {
   model <- glm(formula = formula, data = data, family = "poisson")
@@ -71,7 +70,6 @@ Model_Wappelhorst <- glm(plz5_kba_kraft3 ~
                           , data = plz2017, family = "poisson")
 summary(Model_Wappelhorst)
 
-
 # First explorative analysis
 
 # fit 30 different poisson models of different size
@@ -100,98 +98,56 @@ size.steps <- seq(from = model.size.start, to = model.size.end, by =  model.size
 sample.matrix <- cbind(size.steps, rep(5, length(size.steps)))
 poisson.models <- model_sampler(sample.matrix = sample.matrix, data = plz2021, covariates = covariates2021)
 
-library(tidyverse)
-library(glmnet)
-library(reshape2)
+# defining nested model formulas devided into small, medium and large model formula
+covariates.small <- c("plz5_kba_kraft3", "ort", "plz5_n3","plz5_wfl", "plz5_garage", "plz5_wh_preis_idx", "plz5_dist_opnv", "plz5_ew", "plz5_hh", 
+                     "plz5_absol_hoch", "plz5_kk_ew", "plz5_anz_firm", "plz5_opnv_idx", "plz5_ew_dichte_kl", "plz5_kauf", "plz5_miet")
 
-# importing the data
-data_21 = read.csv2("/Users/ilijaspasojevic/Desktop/infas360_PLZ_Daten_15022024/PLZ_2021.csv", encoding = "latin1")
-data_17 = read.csv2("/Users/ilijaspasojevic/Desktop/infas360_PLZ_Daten_15022024/PLZ_2017.csv", encoding = "latin1")
+covariates.medium <- c(covariates.small, "plz5_adr", "plz5_hh_dinks", "plz5_absol_fach", "plz5_kk_hh", "plz5_eg_ant", 
+                       "plz5_kk_vol", "plz5_solar")
 
-data_21$plz5_miet = as.numeric(data_21$plz5_miet)
-data_21$plz5_eg_ant = as.numeric(data_21$plz5_eg_ant)
+covariates.large <- c(covariates.medium, "plz5_ew_m_ratio", "plz5_lage_gut", "plz5_lage_mittel", "plz5_firma_klein",
+                      "plz5_firma_mittel", "plz5_firma_groß") 
 
-# defining data frames with several numbers of covariables 
-# small small one has most important one then medium hast most important and important one and the large one 
-data_21_small = data_21 %>% 
-  select(c("plz5_kba_kraft3", "ort", "plz5_n3","plz5_wfl", "plz5_garage", "plz5_wh_preis_idx", "plz5_dist_opnv", "plz5_ew", "plz5_hh", 
-           "plz5_absol_hoch", "plz5_kk_ew", "plz5_anz_firm", "plz5_opnv_idx", "plz5_ew_dichte_kl", "plz5_kauf", "plz5_miet"))
-# data_21_small$plz5_miet = as.numeric(data_21_small$plz5_miet)
-
-data_21_medium = data_21 %>% 
-  select(c(colnames(data_21_small), "plz5_adr", "plz5_hh_dinks", "plz5_absol_fach", "plz5_kk_hh", "plz5_eg_ant", 
-           "plz5_kk_vol", "plz5_solar"))
-# data_21_medium$plz5_miet = as.numeric(data_21_medium$plz5_miet)
-# data_21_medium$plz5_eg_ant = as.numeric(data_21_medium$plz5_eg_ant)
-
-# nehme lage schlecht raus da sonst linear abhaengig der Datensatz da lage gut +. mittel + schlecht = anzahl adressen 
-data_21_large = data_21 %>%
-  select(starts_with("plz5_wolage") | plz5_ew_m | starts_with("plz5_firm_ums")) %>%
-  bind_cols(data_21_medium) %>%
-  mutate(plz5_ew_m_ratio = plz5_ew_m / plz5_ew) %>%
-  select(-plz5_ew_m) %>% 
-  mutate(plz5_lage_gut = plz5_wolage1 + plz5_wolage2 + plz5_wolage3, 
-         plz5_lage_mittel = plz5_wolage4 + plz5_wolage5 + plz5_wolage6) %>%
-  select(- starts_with("plz5_wolage")) %>% 
-  mutate(plz5_firma_klein = plz5_firm_ums_kl1 + plz5_firm_ums_kl2 + plz5_firm_ums_kl3, 
-         plz5_firma_mittel = plz5_firm_ums_kl4 + plz5_firm_ums_kl5 + plz5_firm_ums_kl6, 
-         plz5_firma_groß = plz5_firm_ums_kl7 + plz5_firm_ums_kl8 + plz5_firm_ums_kl9 + plz5_firm_ums_kl10) %>% 
-  select(- starts_with("plz5_firm_ums"))
-
-# ersetzte NA's mit dem mean 
-data_21_large = data_21_large %>%
+# ommit 'lage schlecht' in order to avoid issues of linear dependencies
+plz2021 <- plz2021 %>%  mutate(plz5_lage_gut = plz5_wolage1 + plz5_wolage2 + plz5_wolage3, 
+                               plz5_lage_mittel = plz5_wolage4 + plz5_wolage5 + plz5_wolage6) %>%
+                        mutate(plz5_firma_klein = plz5_firm_ums_kl1 + plz5_firm_ums_kl2 + plz5_firm_ums_kl3, 
+                               plz5_firma_mittel = plz5_firm_ums_kl4 + plz5_firm_ums_kl5 + plz5_firm_ums_kl6, 
+                               plz5_firma_groß = plz5_firm_ums_kl7 + plz5_firm_ums_kl8 + plz5_firm_ums_kl9 + plz5_firm_ums_kl10) %>%
+                        mutate(plz5_ew_m_ratio = plz5_ew_m / plz5_ew)
+  
+# Imputation missing values with mean
+plz2021 <- plz2021 %>%
   mutate(plz5_firma_klein = ifelse(is.na(plz5_firma_klein), mean(plz5_firma_klein, na.rm = TRUE), plz5_firma_klein)) %>%
   mutate(plz5_firma_mittel = ifelse(is.na(plz5_firma_mittel), mean(plz5_firma_mittel, na.rm = TRUE), plz5_firma_mittel)) %>%
   mutate(plz5_firma_groß = ifelse(is.na(plz5_firma_groß), mean(plz5_firma_groß, na.rm = TRUE), plz5_firma_groß)) 
 
-# fitting now possion models for the different data sets 
-data_list = list(data_21_small, data_21_medium, data_21_large)
+# fitting possion models for the nested model formulas with number of households as offset
+model.formulas <- lapply(X = list(covariates.small, covariates.medium, covariates.large),
+                         FUN = function(x) get_formula(x, offset = "plz5_hh"))
 model_list = vector(mode = "list", length = 3)
 performance_matrix_2021 = matrix(ncol = 2, nrow = 3)
 colnames(performance_matrix_2021) = c("AIC", "BIC")
 rownames(performance_matrix_2021) = c("small", "medium", "large")
+
 for (i in 1:3) {
-  fit = glm(plz5_kba_kraft3 ~. , data = data_list[[i]], family = poisson())
+  fit = poisson_fit(formula = model.formulas[[i]], data = plz2021)
   model_list[[i]] = fit 
   performance_matrix_2021[i,1] = AIC(fit)
   performance_matrix_2021[i,2] = BIC(fit)
 }
 
 performance_matrix_2021
-# Nach AIC und BIC ist small am besten dann medium und performance von large am schelchtesten 
 
-# Dies wart lediglich der Anfang rein über Intuation jeztzt veruche es mit Lasso etc zuselecktieren 
-model_lasso = glmnet(x = as.matrix(data_21_large[,-1]), y= data_21_large$plz5_kba_kraft3, alpha = 1) 
-sum_lasso = summary(model_lasso)
-lambda_lasso = cv.glmnet(x = as.matrix(data_21_large[, -1]), 
-                         y = data_21_large$plz5_kba_kraft3)
+# Run variable selection with lasso
+model_lasso <- glmnet(x = plz2021[, covariates.large], y = plz2021$plz5_kba_kraft3 / plz2021$plz,
+                      alpha = 1)
+sum_lasso <- summary(model_lasso)
+
+lambda_lasso <- cv.glmnet(x = as.matrix(plz2021[, covariates.large]), y = plz2021$plz5_kba_kraft3)
 coef(model_lasso, s = lambda_lasso)
 
-str(data_21_large)
-
-
-
-
-data_list = list(data_21_small, data_21_medium, data_21_large)
-model_list = vector(mode = "list", length = 3)
-performance_matrix_2021 = matrix(ncol = 2, nrow = 3)
-colnames(performance_matrix_2021) = c("AIC", "BIC")
-rownames(performance_matrix_2021) = c("small", "medium", "large")
-for (i in 1:3) {
-  start_values <- rep(0, ncol(data_list[[i]]) )
-  
-  fit = glm(plz5_kba_kraft3 ~., offset = plz5_ew ,data = data_list[[i]], family = poisson(), start = start_values)
-  model_list[[i]] = fit 
-  performance_matrix_2021[i,1] = AIC(fit)
-  performance_matrix_2021[i,2] = BIC(fit)
-}
-
-performance_matrix_2021
-
-summary(model_list[[1]])
-
-
-correlation_matrix = cor(data_21_large[, -2], use = "pairwise.complete.obs")
+correlation_matrix = cor(plz2021[, covariates.large[-2]], use = "pairwise.complete.obs")
 
 # Melt correlation matrix
 melted_correlation <- melt(correlation_matrix)
